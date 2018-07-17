98c423dcf30cedb6f1d10a61a3e73eb0b307d84e
Ian Vaughan <ian@quiqup.com>
2018-07-17 17:25:06 +0100

    Allow client to configure the log level
    Locally we can get a lot of noise:
    
        17:23:10.571 [debug] [libelection][elector] No connected nodes. Leader unchanged :nonode@nohost
    
        17:23:11.572 [debug] [libelection][elector] No connected nodes. Leader unchanged :nonode@nohost
    
        17:23:12.573 [debug] [libelection][elector] No connected nodes. Leader unchanged :nonode@nohost

--- lib/logger.ex
+++ lib/logger.ex
@@ -5,9 +5,23 @@ defmodule Election.Logger do
     quote do
       require Logger
 
-      def log(level, tag, fun) when is_function(fun), do: Logger.log(level, "#{tags(tag)} #{fun.()}")
-      def log(level, tag, message), do: Logger.log(level, "#{tags(tag)} #{message}")
+      def log(level, tag, fun) when is_function(fun), do: maybe_log(level, "#{tags(tag)} #{fun.()}")
+      def log(level, tag, message), do: maybe_log(level, "#{tags(tag)} #{message}")
       defp tags(tag), do: "[libelection][#{tag}]"
+
+      defp maybe_log(level, message) do
+        case Logger.compare_levels(log_level(), level) do
+          :gt -> nil
+          _ -> Logger.log(level, message)
+        end
+      end
+
+      defp log_level do
+        case Application.get_env(:libelection, :logger) do
+          %{level: level} -> level
+          _ -> :debug
+        end
+      end
     end
   end
 end
