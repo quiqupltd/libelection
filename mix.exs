1a073b4cf551efbd695796704bc80c4aff10c593
Luiz Varela <luiz@quiqup.com>
2019-04-05 17:54:52 +0200

    Update ExDoc

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Libelection.Mixfile do
   def project do
     [
       app: :libelection,
-      version: "0.3.0",
-      elixir: "~> 1.4",
+      version: "1.0.0",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description:
@@ -43,7 +43,7 @@ defmodule Libelection.Mixfile do
   defp deps do
     [
       {:poison, "~> 3.0"},
-      {:ex_doc, "~> 0.13", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:dialyxir, "~> 0.3", only: :dev},
       {:credo, "~> 0.8", only: [:dev, :test]},
       {:bypass, github: "PSPDFKit-labs/bypass", only: :test},
