d902c33c3d2abcccf58d2e4e5edaa244e6344fea
Danny Hawkins <danny@quiqup.com>
2023-05-19 11:41:24 +0400

    wip

--- lib/strategy/kubernetes.ex
+++ lib/strategy/kubernetes.ex
@@ -22,84 +22,107 @@ defmodule Election.Strategy.Kubernetes do
   def leader(connected_nodes, config) do
     case discover_nodes(config) do
       [] ->
-        log :error, :kubernetes, "No connected nodes"
+        log(:error, :kubernetes, "No connected nodes")
 
         nil
-      [_|_] = nodes ->
-        with %{node: leader_node, create_index: create_index} <- nodes
-                                                                 |> Enum.filter(&(&1.node in connected_nodes))
-                                                                 |> oldest_node do
+
+      [_ | _] = nodes ->
+        with %{node: leader_node, create_index: create_index} <-
+               nodes
+               |> Enum.filter(&(&1.node in connected_nodes))
+               |> oldest_node do
           %{node: leader_node, metadata: %{create_index: create_index}}
         end
+
       {:error, reason} ->
-        log :error, :kubernetes, "Failed to fetch nodes with error: #{inspect reason}"
+        log(:error, :kubernetes, "Failed to fetch nodes with error: #{inspect(reason)}")
 
         nil
+
       _ ->
-        log :error, :kubernetes, "Unexpected response from Kubernetes API"
+        log(:error, :kubernetes, "Unexpected response from Kubernetes API")
 
         nil
     end
   end
 
   defp discover_nodes(config) do
-    with %{app_name: app_name, selector: selector} when not(is_nil(app_name)) and not(is_nil(selector)) <- %{
-        app_name: Access.get(config, :kubernetes_node_basename),
-        selector: Access.get(config, :kubernetes_selector)
-      },
-      endpoints_path <- 'api/v1/namespaces/#{namespace()}/endpoints?labelSelector=#{selector}',
-      url <- '#{base_url(config)}/#{endpoints_path}',
-      {:response, {:ok, body}} <- {:response, api_request(url)} do
+    with %{app_name: app_name, selector: selector}
+         when not is_nil(app_name) and not is_nil(selector) <- %{
+           app_name: Access.get(config, :kubernetes_node_basename),
+           selector: Access.get(config, :kubernetes_selector)
+         },
+         endpoints_path <- 'api/v1/namespaces/#{namespace()}/endpoints?labelSelector=#{selector}',
+         url <- '#{base_url(config)}/#{endpoints_path}',
+         {:response, {:ok, body}} <- {:response, api_request(url)} do
       parse_response(app_name, Poison.decode!(body))
     else
       %{app_name: app_name, selector: selector} when is_nil(app_name) or is_nil(selector) ->
-        log :warn, :kubernetes, "configuration value is nil, config: #{inspect config}"
+        log(:warn, :kubernetes, "configuration value is nil, config: #{inspect(config)}")
         {:error, :invalud_configuration}
-      {:response, {:error, %{message: :invalid_response_status, code: code, status: status, body: body}}} ->
-        log :warn, :kubernetes, "cannot query kubernetes (#{code} #{status}): #{inspect body}"
+
+      {:response,
+       {:error, %{message: :invalid_response_status, code: code, status: status, body: body}}} ->
+        log(:warn, :kubernetes, "cannot query kubernetes (#{code} #{status}): #{inspect(body)}")
+
       {:response, {:error, %{message: :unauthorized, body: body}}} ->
-        log :warn, :kubernetes, "cannot query kubernetes (unauthorized): #{inspect body}"
+        log(:warn, :kubernetes, "cannot query kubernetes (unauthorized): #{inspect(body)}")
         {:error, :unauthorized}
-      {:response, other} -> {:error, :invalid_response}
+
+      {:response, other} ->
+        {:error, :invalid_response}
     end
   end
 
   defp parse_response(app_name, resp) do
     case resp do
-      %{"items" => []} -> []
+      %{"items" => []} ->
+        []
+
       %{"items" => items} ->
         Enum.reduce(items, [], fn
-          %{"subsets" => []}, acc -> acc
+          %{"subsets" => []}, acc ->
+            acc
+
           %{"subsets" => subsets}, acc ->
-            addrs = Enum.flat_map(subsets, fn
-              %{"addresses" => addresses} ->
-                # credo:disable-for-next-line Credo.Check.Refactor.Nesting
-                Enum.map(addresses, fn %{"ip" => ip, "targetRef" => %{"resourceVersion" => version}} ->
-                  %{create_index: String.to_integer(version), node: :"#{app_name}@#{ip}"}
-                end)
-              _ -> []
-            end)
+            addrs =
+              Enum.flat_map(subsets, fn
+                %{"addresses" => addresses} ->
+                  # credo:disable-for-next-line Credo.Check.Refactor.Nesting
+                  Enum.map(addresses, fn %{"ip" => ip, "targetRef" => targetRef} ->
+                    %{create_index: resource_version(targetRef), node: :"#{app_name}@#{ip}"}
+                  end)
+
+                _ ->
+                  []
+              end)
+
             acc ++ addrs
+
           _, acc ->
             acc
         end)
+
       _ ->
         []
     end
   end
 
-  @spec token() :: String.t
+  defp resource_version(%{"resourceVersion" => version}), do: String.to_integer(version)
+  defp resource_version(_), do: 1
+
+  @spec token() :: String.t()
   defp token, do: service_file("token")
 
-  @spec namespace() :: String.t
+  @spec namespace() :: String.t()
   defp namespace, do: service_file("namespace")
 
-  @spec service_file(String.t) :: String.t
+  @spec service_file(String.t()) :: String.t()
   defp service_file(filename) do
     path = Path.join(@service_account_path, filename)
 
     case File.exists?(path) do
-      true  -> path |> File.read! |> String.trim()
+      true -> path |> File.read!() |> String.trim()
       false -> ""
     end
   end
@@ -109,17 +132,22 @@ defmodule Election.Strategy.Kubernetes do
     http_options = [ssl: [verify: :verify_none]]
 
     case :httpc.request(:get, {url, headers}, http_options, []) do
-      {:ok, {{_version, 200, _status}, _headers, body}} -> {:ok, body}
+      {:ok, {{_version, 200, _status}, _headers, body}} ->
+        {:ok, body}
+
       {:ok, {{_version, 403, _status}, _headers, body}} ->
         {:error, %{message: :unauthorized, body: Poison.decode!(body)}}
+
       {:ok, {{_version, code, status}, _headers, body}} ->
         {:error, %{message: :invalid_response_status, code: code, status: status, body: body}}
-      {:error, reason} -> {:error, reason}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
   defp oldest_node([]), do: nil
-  defp oldest_node([_|_] = nodes), do: nodes |> Enum.min_by(&(&1.create_index))
+  defp oldest_node([_ | _] = nodes), do: nodes |> Enum.min_by(& &1.create_index)
 
   defp base_url(%{kubernetes_api_base_url: url}), do: url
   defp base_url(_), do: @kubernetes_api_base_url
--- test/strategy/kubernetes_test.exs
+++ test/strategy/kubernetes_test.exs
@@ -11,122 +11,234 @@ defmodule Election.Strategy.KubernetesTest do
 
   describe "leader/2 without connected nodes" do
     setup do
-      {:ok, %{subject: &(Subject.leader([], &1))}}
+      {:ok, %{subject: &Subject.leader([], &1)}}
     end
 
     test "when fetching nodes fails with a 500 error, returns nil", %{subject: subject} do
       bypass = Bypass.open()
+
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 500, "Some error message")
       end)
 
-      log = capture_log fn ->
-        assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) == nil
-      end
+      log =
+        capture_log(fn ->
+          assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==
+                   nil
+        end)
 
       assert log =~ "cannot query kubernetes (500 Internal Server Error)"
     end
 
     test "when fetching nodes fails due to authorization error, returns nil", %{subject: subject} do
       bypass = Bypass.open()
+
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 403, "{\"message\": \"Authorization failed\"}")
       end)
 
-      log = capture_log fn ->
-        assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) == nil
-      end
+      log =
+        capture_log(fn ->
+          assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==
+                   nil
+        end)
 
       assert log =~ "cannot query kubernetes (unauthorized)"
     end
 
-    test "when other nodes are discovered but not included in the connected nodes, returns nil", %{subject: subject} do
+    test "when other nodes are discovered but not included in the connected nodes, returns nil",
+         %{subject: subject} do
       bypass = Bypass.open()
+
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 200, Poison.encode!(fixture(:kubernetes_api_response)))
       end)
 
-      assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) == nil
+      assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==
+               nil
     end
   end
 
   describe "leader/2 with connected nodes" do
     setup do
-      nodes = [:'example@192.168.1.1', :'example@192.168.1.2', :'example@192.168.1.3']
-      {:ok, %{subject: &(Subject.leader(nodes, &1))}}
+      nodes = [:"example@192.168.1.1", :"example@192.168.1.2", :"example@192.168.1.3"]
+      {:ok, %{subject: &Subject.leader(nodes, &1)}}
     end
 
     test "when fetching nodes fails with a 500 error, returns nil", %{subject: subject} do
       bypass = Bypass.open()
+
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 500, "Some error message")
       end)
 
-      log = capture_log fn ->
-        assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) == nil
-      end
+      log =
+        capture_log(fn ->
+          assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==
+                   nil
+        end)
 
       assert log =~ "cannot query kubernetes (500 Internal Server Error)"
     end
 
     test "when fetching nodes fails due to authorization error, returns nil", %{subject: subject} do
       bypass = Bypass.open()
+
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 403, "{\"message\": \"Authorization failed\"}")
       end)
 
-      log = capture_log fn ->
-        assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) == nil
-      end
+      log =
+        capture_log(fn ->
+          assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==
+                   nil
+        end)
 
       assert log =~ "cannot query kubernetes (unauthorized)"
     end
 
-    test "when other nodes are discovered, returns the node with the lowest resourceVersion", %{subject: subject} do
+    test "when other nodes are discovered, returns the node with the lowest resourceVersion", %{
+      subject: subject
+    } do
       bypass = Bypass.open()
+
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 200, Poison.encode!(fixture(:kubernetes_api_response)))
       end)
 
       expectation = %{metadata: %{create_index: 80}, node: :"example@192.168.1.2"}
 
-      assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==  expectation
+      assert subject.(put_in(@config[:kubernetes_api_base_url], kubernetes_base_url(bypass))) ==
+               expectation
     end
   end
 
-  defp kubernetes_base_url(%{port: port}), do: "http://localhost:#{port}" |> String.to_charlist
+  defp kubernetes_base_url(%{port: port}), do: "http://localhost:#{port}" |> String.to_charlist()
 
   defp fixture(:kubernetes_api_response) do
     %{
       "apiVersion" => "v1",
-      "items" => [%{"metadata" => %{"creationTimestamp" => "2018-01-11T13:03:10Z",
-        "labels" => %{"app" => "example", "strategy" => "full"},
-        "name" => "example", "namespace" => "default",
-        "resourceVersion" => "5127743",
-        "selfLink" => "/api/v1/namespaces/default/endpoints/example",
-        "uid" => "c64dc5ff-f6cf-11e7-a513-42010a9c01f0"},
-        "subsets" => [%{
-          "addresses" => [%{"ip" => "192.168.1.1",
-          "nodeName" => "some-node-name",
-          "targetRef" => %{"kind" => "Pod",
-            "name" => "example-4204218243-7g0pr", "namespace" => "default",
-            "resourceVersion" => "100",
-            "uid" => "1a42feb7-0ffd-11e8-bc95-42010a9c0154"}},
-               %{"ip" => "192.168.1.2",
-                 "nodeName" => "some-node-name",
-                 "targetRef" => %{"kind" => "Pod",
-                   "name" => "example-4204218243-98sh4", "namespace" => "default",
-                   "resourceVersion" => "80",
-                   "uid" => "3e32b139-0ffd-11e8-bc95-42010a9c0154"}},
-               %{"ip" => "192.168.1.3",
-                 "nodeName" => "some0-node-name",
-                 "targetRef" => %{"kind" => "Pod",
-                   "name" => "example-4204218243-n1t2r", "namespace" => "default",
-                   "resourceVersion" => "120",
-                   "uid" => "fc60f592-0ffc-11e8-bc95-42010a9c0154"}}],
-          "ports" => [%{"port" => 4000, "protocol" => "TCP"}]}]}],
+      "items" => [
+        %{
+          "metadata" => %{
+            "creationTimestamp" => "2018-01-11T13:03:10Z",
+            "labels" => %{"app" => "example", "strategy" => "full"},
+            "name" => "example",
+            "namespace" => "default",
+            "resourceVersion" => "5127743",
+            "selfLink" => "/api/v1/namespaces/default/endpoints/example",
+            "uid" => "c64dc5ff-f6cf-11e7-a513-42010a9c01f0"
+          },
+          "subsets" => [
+            %{
+              "addresses" => [
+                %{
+                  "ip" => "192.168.1.1",
+                  "nodeName" => "some-node-name",
+                  "targetRef" => %{
+                    "kind" => "Pod",
+                    "name" => "example-4204218243-7g0pr",
+                    "namespace" => "default",
+                    "resourceVersion" => "100",
+                    "uid" => "1a42feb7-0ffd-11e8-bc95-42010a9c0154"
+                  }
+                },
+                %{
+                  "ip" => "192.168.1.2",
+                  "nodeName" => "some-node-name",
+                  "targetRef" => %{
+                    "kind" => "Pod",
+                    "name" => "example-4204218243-98sh4",
+                    "namespace" => "default",
+                    "resourceVersion" => "80",
+                    "uid" => "3e32b139-0ffd-11e8-bc95-42010a9c0154"
+                  }
+                },
+                %{
+                  "ip" => "192.168.1.3",
+                  "nodeName" => "some0-node-name",
+                  "targetRef" => %{
+                    "kind" => "Pod",
+                    "name" => "example-4204218243-n1t2r",
+                    "namespace" => "default",
+                    "resourceVersion" => "120",
+                    "uid" => "fc60f592-0ffc-11e8-bc95-42010a9c0154"
+                  }
+                }
+              ],
+              "ports" => [%{"port" => 4000, "protocol" => "TCP"}]
+            }
+          ]
+        }
+      ],
       "kind" => "EndpointsList",
-      "metadata" => %{"resourceVersion" => "5143116", "selfLink" => "/api/v1/namespaces/default/endpoints"}
+      "metadata" => %{
+        "resourceVersion" => "5143116",
+        "selfLink" => "/api/v1/namespaces/default/endpoints"
+      }
+    }
+  end
+
+  defp fixture(:kubernetes_api_response_no_resource_version) do
+    %{
+      "apiVersion" => "v1",
+      "items" => [
+        %{
+          "metadata" => %{
+            "creationTimestamp" => "2018-01-11T13:03:10Z",
+            "labels" => %{"app" => "example", "strategy" => "full"},
+            "name" => "example",
+            "namespace" => "default",
+            "resourceVersion" => "5127743",
+            "selfLink" => "/api/v1/namespaces/default/endpoints/example",
+            "uid" => "c64dc5ff-f6cf-11e7-a513-42010a9c01f0"
+          },
+          "subsets" => [
+            %{
+              "addresses" => [
+                %{
+                  "ip" => "192.168.1.1",
+                  "nodeName" => "some-node-name",
+                  "targetRef" => %{
+                    "kind" => "Pod",
+                    "name" => "example-4204218243-7g0pr",
+                    "namespace" => "default",
+                    "resourceVersion" => "100",
+                    "uid" => "1a42feb7-0ffd-11e8-bc95-42010a9c0154"
+                  }
+                },
+                %{
+                  "ip" => "192.168.1.2",
+                  "nodeName" => "some-node-name",
+                  "targetRef" => %{
+                    "kind" => "Pod",
+                    "name" => "example-4204218243-98sh4",
+                    "namespace" => "default",
+                    "uid" => "3e32b139-0ffd-11e8-bc95-42010a9c0154"
+                  }
+                },
+                %{
+                  "ip" => "192.168.1.3",
+                  "nodeName" => "some0-node-name",
+                  "targetRef" => %{
+                    "kind" => "Pod",
+                    "name" => "example-4204218243-n1t2r",
+                    "namespace" => "default",
+                    "resourceVersion" => "120",
+                    "uid" => "fc60f592-0ffc-11e8-bc95-42010a9c0154"
+                  }
+                }
+              ],
+              "ports" => [%{"port" => 4000, "protocol" => "TCP"}]
+            }
+          ]
+        }
+      ],
+      "kind" => "EndpointsList",
+      "metadata" => %{
+        "resourceVersion" => "5143116",
+        "selfLink" => "/api/v1/namespaces/default/endpoints"
+      }
     }
   end
 end
